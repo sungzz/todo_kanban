ac74cfddc6bbd95fb5f547067496cd9b302028ab
sung <khisungz@gmail.com>
2018-07-25 14:42:07 +0700

    Chapter 9 Kanban still many code not learn for validate

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'To-do Kanban',
+    'description': 'Kanban board for to-do tasks.',
+    'author': 'Santoni',
+    'depends': ['todo_ui'],
+    'data':[
+        'views/todo_view.xml'
+    ],
+    'application': True
+
+}
